434242c1498f88216d2e5a8874ec20ca308d86c8
FeyziPC <mert.erim@ozu.edu.tr>
2023-12-13 00:10:10 +0300

    commit

--- app/src/main/java/com/example/project1/Profile.kt
+++ app/src/main/java/com/example/project1/Profile.kt
@@ -34,7 +34,7 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basket, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
     companion object {
